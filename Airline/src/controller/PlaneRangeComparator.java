bc3ecb7da72d4eb3a1a7e64cd9d0c643fe5b4722
Valeryia Zhdanava <Valeryia_Zhdanava@epam.com>
2014-11-10 23:34:30 +0300

    delete the other a function

--- Airline/src/controller/PlaneRangeComparator.java
+++ Airline/src/controller/PlaneRangeComparator.java
@@ -5,17 +5,23 @@ import java.util.Comparator;
 import entity.Plane;
 
 public class PlaneRangeComparator implements Comparator<Plane> {
-	public int compare(Plane p1, Plane p2) {
-		int range1 = p1.getRange();
-		int range2 = p2.getRange();
 
-		if (range1 < range2) {
-			return -1;
-		} else if (range1 == range2) {
-			return 0;
-		} else {
-			return 1;
-		}
+	@Override
+	public int compare(Plane arg0, Plane arg1) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
+//	public int compare(Plane p1, Plane p2) {
+//		int range1 = p1.getRange();
+//		int range2 = p2.getRange();
+//
+//		if (range1 < range2) {
+//			return -1;
+//		} else if (range1 == range2) {
+//			return 0;
+//		} else {
+//			return 1;
+//		}
+//	}
 
 }
