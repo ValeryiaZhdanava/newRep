01856e8a1a2beff8fb64007878c31b353e822bd6
Valeryia Zhdanava <Valeryia_Zhdanava@epam.com>
2014-11-10 23:31:32 +0300

    delete one function

--- Airline/src/controller/PlaneFuelComparator.java
+++ Airline/src/controller/PlaneFuelComparator.java
@@ -7,17 +7,23 @@ import entity.Plane;
 public class PlaneFuelComparator implements Comparator<Plane> {
 
 	@Override
-	public int compare(Plane p1, Plane p2) {
-
-		int fuel1 = p1.getFuelConsumption();
-		int fuel2 = p2.getFuelConsumption();
-
-		if (fuel1 < fuel2) {
-			return -1;
-		} else if (fuel1 == fuel2) {
-			return 0;
-		} else {
-			return 1;
-		}
+	public int compare(Plane arg0, Plane arg1) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
+
+//	@Override
+//	public int compare(Plane p1, Plane p2) {
+//
+//		int fuel1 = p1.getFuelConsumption();
+//		int fuel2 = p2.getFuelConsumption();
+//
+//		if (fuel1 < fuel2) {
+//			return -1;
+//		} else if (fuel1 == fuel2) {
+//			return 0;
+//		} else {
+//			return 1;
+//		}
+//	}
 }
